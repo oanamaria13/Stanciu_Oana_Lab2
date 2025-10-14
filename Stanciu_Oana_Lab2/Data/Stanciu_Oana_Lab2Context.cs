f1c8d2a76ff86e4748632a65449126debc3c3ac6
OANA-MARIA STANCIU <oana.stanciu@stud.ubbcluj.ro>
2025-10-14 15:55:31 +0300

    Add project files.

---
+++ Stanciu_Oana_Lab2/Data/Stanciu_Oana_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stanciu_Oana_Lab2.Models;
+
+namespace Stanciu_Oana_Lab2.Data
+{
+    public class Stanciu_Oana_Lab2Context : DbContext
+    {
+        public Stanciu_Oana_Lab2Context (DbContextOptions<Stanciu_Oana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stanciu_Oana_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Stanciu_Oana_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Stanciu_Oana_Lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stanciu_Oana_Lab2.Data;
+using Stanciu_Oana_Lab2.Models;
+
+namespace Stanciu_Oana_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stanciu_Oana_Lab2.Data.Stanciu_Oana_Lab2Context _context;
+
+        public IndexModel(Stanciu_Oana_Lab2.Data.Stanciu_Oana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+               .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
